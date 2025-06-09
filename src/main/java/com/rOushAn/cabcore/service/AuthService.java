db660b4dc8f87be99a02e41f26daae360587b775
Roushan Kumar <rk04393@gmail.com>
2025-06-09 15:28:12 +0530

    completely implemented AuthServiceImpl

--- src/main/java/com/rOushAn/cabcore/service/AuthService.java
+++ src/main/java/com/rOushAn/cabcore/service/AuthService.java
@@ -1,9 +1,6 @@
 package com.rOushAn.cabcore.service;
 
-import com.rOushAn.cabcore.dtos.LoginRequestDto;
-import com.rOushAn.cabcore.dtos.LoginResponseDto;
-import com.rOushAn.cabcore.dtos.SignupDto;
-import com.rOushAn.cabcore.dtos.UserDto;
+import com.rOushAn.cabcore.dtos.*;
 import org.springframework.transaction.annotation.Transactional;
 
 public interface AuthService {
@@ -11,6 +8,9 @@ public interface AuthService {
 
     @Transactional
     UserDto signup(SignupDto signupDto);
+
+    @Transactional
+    DriverDto onboardNewDriver(Long userId, OnboardDriverDto onboardDriverDto);
 
     LoginResponseDto refreshToken(String refreshToken);
 }
--- src/main/java/com/rOushAn/cabcore/service/implementations/AuthServiceImpl.java
+++ src/main/java/com/rOushAn/cabcore/service/implementations/AuthServiceImpl.java
@@ -1,31 +1,51 @@
 package com.rOushAn.cabcore.service.implementations;
 
-import com.rOushAn.cabcore.dtos.LoginRequestDto;
-import com.rOushAn.cabcore.dtos.LoginResponseDto;
-import com.rOushAn.cabcore.dtos.SignupDto;
-import com.rOushAn.cabcore.dtos.UserDto;
+import com.rOushAn.cabcore.dtos.*;
+import com.rOushAn.cabcore.entities.Driver;
 import com.rOushAn.cabcore.entities.User;
+import com.rOushAn.cabcore.entities.enums.Roles;
+import com.rOushAn.cabcore.exceptions.ResourceNotFoundException;
+import com.rOushAn.cabcore.exceptions.RuntimeConflictException;
+import com.rOushAn.cabcore.repositories.UserRepository;
 import com.rOushAn.cabcore.security.JwtService;
-import com.rOushAn.cabcore.service.AuthService;
-import com.rOushAn.cabcore.service.UserService;
+import com.rOushAn.cabcore.service.*;
+import com.rOushAn.cabcore.utils.GeometryUtil;
+import org.locationtech.jts.geom.Point;
+import org.modelmapper.ModelMapper;
 import org.springframework.security.authentication.AuthenticationCredentialsNotFoundException;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
+import java.util.Set;
 
 @Service
 public class AuthServiceImpl implements AuthService {
 
+    private final ModelMapper modelMapper;
+    private final UserRepository userRepository;
+    private final RiderService riderService;
+    private final WalletService walletService;
+    private final DriverService driverService;
+    private final PasswordEncoder passwordEncoder;
+    private final AuthenticationManager authenticationManager;
     private final JwtService jwtService;
     private final UserService userService;
-    private final AuthenticationManager authenticationManager;
 
-    public AuthServiceImpl(JwtService jwtService, UserService userService, AuthenticationManager authenticationManager) {
+    public AuthServiceImpl(ModelMapper modelMapper, AuthenticationManager authenticationManager, PasswordEncoder passwordEncoder, UserService userService, JwtService jwtService, UserRepository userRepository, DriverService driverService, RiderService riderService, WalletService walletService) {
+        this.modelMapper = modelMapper;
+        this.userRepository = userRepository;
+        this.riderService = riderService;
+        this.walletService = walletService;
+        this.driverService = driverService;
+        this.passwordEncoder = passwordEncoder;
+        this.authenticationManager = authenticationManager;
         this.jwtService = jwtService;
         this.userService = userService;
-        this.authenticationManager = authenticationManager;
     }
 
     @Override
@@ -43,12 +63,50 @@ public class AuthServiceImpl implements AuthService {
         return new String[]{accessToken, refreshToken};
     }
 
-    @Transactional
     @Override
+    @Transactional
     public UserDto signup(SignupDto signupDto) {
-        return null;
+        Optional<User> user = userRepository.findByEmail(signupDto.getEmail());
+        if (user.isPresent()) {
+            throw new RuntimeConflictException("User already exists with that email!");
+        }
+        User mappedUser = modelMapper.map(signupDto, User.class);
+        mappedUser.setPassword(passwordEncoder.encode(mappedUser.getPassword()));
+        mappedUser.setRoles(Set.of(Roles.RIDER));
+        User savedUser = userRepository.save(mappedUser);
+        riderService.createNewRider(savedUser);
+        walletService.createNewWallet(savedUser);
+        return modelMapper.map(savedUser, UserDto.class);
     }
 
+    @Transactional
+    @Override
+    public DriverDto onboardNewDriver(Long userId, OnboardDriverDto onboardDriverDto) {
+
+        User user = userRepository
+                .findById(userId)
+                .orElseThrow(() -> new ResourceNotFoundException("No user was found with ID: " + userId));
+
+        if (user.getRoles().contains(Roles.DRIVER)) {
+            throw new RuntimeConflictException("User is already a driver!");
+        }
+
+        Point currentLocation = GeometryUtil.createPoint(onboardDriverDto.getCurrentLocation());
+        Driver createDriver = new Driver
+                .DriverBuilder()
+                .available(true)
+                .rating(0.0)
+                .user(user)
+                .vehicleId(onboardDriverDto.getVehicleId())
+                .currentLocation(currentLocation)
+                .build();
+
+        user.getRoles().add(Roles.DRIVER);
+        userRepository.save(user);
+        Driver savedDriver = driverService.createNewDriver(createDriver);
+
+        return modelMapper.map(savedDriver, DriverDto.class);
+    }
 
     @Override
     public LoginResponseDto refreshToken(String refreshToken) {
