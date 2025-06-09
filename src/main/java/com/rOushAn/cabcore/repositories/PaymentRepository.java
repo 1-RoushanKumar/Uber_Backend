378a8866d3373ff9ce76630ce7ae9a5040d52298
Roushan Kumar <rk04393@gmail.com>
2025-06-09 15:17:31 +0530

    implemented PaymentServiceImpl

--- src/main/java/com/rOushAn/cabcore/repositories/PaymentRepository.java
+++ src/main/java/com/rOushAn/cabcore/repositories/PaymentRepository.java
@@ -1,9 +1,13 @@
 package com.rOushAn.cabcore.repositories;
 
 import com.rOushAn.cabcore.entities.Payment;
+import com.rOushAn.cabcore.entities.Ride;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface PaymentRepository extends JpaRepository<Payment, Long> {
+    Optional<Payment> findByRide(Ride ride);
 }
--- src/main/java/com/rOushAn/cabcore/service/implementations/PaymentServiceImpl.java
+++ src/main/java/com/rOushAn/cabcore/service/implementations/PaymentServiceImpl.java
@@ -3,22 +3,52 @@ package com.rOushAn.cabcore.service.implementations;
 import com.rOushAn.cabcore.entities.Payment;
 import com.rOushAn.cabcore.entities.Ride;
 import com.rOushAn.cabcore.entities.enums.PaymentStatus;
+import com.rOushAn.cabcore.exceptions.ResourceNotFoundException;
+import com.rOushAn.cabcore.repositories.PaymentRepository;
 import com.rOushAn.cabcore.service.PaymentService;
+import com.rOushAn.cabcore.strategies.strategyManagers.PaymentStrategyManager;
 import org.springframework.stereotype.Service;
 
 @Service
 public class PaymentServiceImpl implements PaymentService {
 
+    private final PaymentRepository paymentRepository;
+    private final PaymentStrategyManager paymentStrategyManager;
+
+    public PaymentServiceImpl(PaymentRepository paymentRepository, PaymentStrategyManager paymentStrategyManager) {
+        this.paymentRepository = paymentRepository;
+        this.paymentStrategyManager = paymentStrategyManager;
+    }
+
     @Override
     public void processPayment(Ride ride) {
+
+        Payment payment = paymentRepository.findByRide(ride).orElseThrow(() -> new ResourceNotFoundException("Payment not found!"));
+
+        paymentStrategyManager
+                .paymentStrategy(payment.getPaymentMethod())
+                .processPayment(payment);
+
+        updatePaymentStatus(payment, PaymentStatus.CONFIRMED);
     }
 
     @Override
     public Payment createNewPayment(Ride ride) {
-        return null;
+
+        Payment payment = new Payment
+                .PaymentBuilder()
+                .amount(ride.getFare())
+                .paymentMethod(ride.getPaymentMethod())
+                .paymentStatus(PaymentStatus.PENDING)
+                .ride(ride)
+                .build();
+
+        return paymentRepository.save(payment);
     }
 
     @Override
     public void updatePaymentStatus(Payment payment, PaymentStatus paymentStatus) {
+        payment.setPaymentStatus(paymentStatus);
+        paymentRepository.save(payment);
     }
 }
