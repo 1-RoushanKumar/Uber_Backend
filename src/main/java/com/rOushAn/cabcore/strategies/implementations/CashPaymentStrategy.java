456ec1c3ca78b6aa1ef87379213696958da1d45e
Roushan Kumar <rk04393@gmail.com>
2025-06-09 13:57:29 +0530

    implemented CashPaymentStrategy and WalletPaymentStrategy

--- src/main/java/com/rOushAn/cabcore/strategies/implementations/CashPaymentStrategy.java
+++ src/main/java/com/rOushAn/cabcore/strategies/implementations/CashPaymentStrategy.java
@@ -1,14 +1,32 @@
 package com.rOushAn.cabcore.strategies.implementations;
 
+import com.rOushAn.cabcore.entities.Driver;
 import com.rOushAn.cabcore.entities.Payment;
+import com.rOushAn.cabcore.entities.enums.TransactionMethod;
+import com.rOushAn.cabcore.service.WalletService;
 import com.rOushAn.cabcore.strategies.PaymentStrategy;
 import org.springframework.stereotype.Service;
 
 @Service
 public class CashPaymentStrategy implements PaymentStrategy {
 
+    private final WalletService walletService;
+
+    public CashPaymentStrategy(WalletService walletService) {
+        this.walletService = walletService;
+    }
+
     @Override
     public void processPayment(Payment payment) {
 
+        Driver driver = payment.getRide().getDriver();
+        double paymentCommission = payment.getAmount() * PLATFORM_FEE;
+
+        walletService.subtractMoneyFromWallet(driver.getUser(),
+                paymentCommission,
+                null, // Cash on delivery payments do not have transaction ids
+                payment.getRide(),
+                TransactionMethod.RIDE);
     }
 }
+
--- src/main/java/com/rOushAn/cabcore/strategies/implementations/WalletPaymentStrategy.java
+++ src/main/java/com/rOushAn/cabcore/strategies/implementations/WalletPaymentStrategy.java
@@ -1,6 +1,10 @@
 package com.rOushAn.cabcore.strategies.implementations;
 
+import com.rOushAn.cabcore.entities.Driver;
 import com.rOushAn.cabcore.entities.Payment;
+import com.rOushAn.cabcore.entities.Rider;
+import com.rOushAn.cabcore.entities.enums.TransactionMethod;
+import com.rOushAn.cabcore.service.WalletService;
 import com.rOushAn.cabcore.strategies.PaymentStrategy;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -8,9 +12,32 @@ import org.springframework.transaction.annotation.Transactional;
 @Service
 public class WalletPaymentStrategy implements PaymentStrategy {
 
+    private final WalletService walletService;
+
+    public WalletPaymentStrategy(WalletService walletService) {
+        this.walletService = walletService;
+    }
+
     @Override
     @Transactional
     public void processPayment(Payment payment) {
+
+        Driver driver = payment.getRide().getDriver();
+        Rider rider = payment.getRide().getRider();
+
+        double paymentAddedToWallet = payment.getAmount() - (payment.getAmount() * PLATFORM_FEE);
+
+        walletService.addMoneyToWallet(driver.getUser(),
+                paymentAddedToWallet,
+                null,
+                payment.getRide(),
+                TransactionMethod.RIDE);
+
+        walletService.subtractMoneyFromWallet(rider.getUser(),
+                payment.getAmount(),
+                null,
+                payment.getRide(),
+                TransactionMethod.RIDE);
 
     }
 }
