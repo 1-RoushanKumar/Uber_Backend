abf8d4251e88fefcc518f335855e2f671cfb145e
Roushan Kumar <rk04393@gmail.com>
2025-06-09 14:59:56 +0530

    implemented the RatingManagementServiceImpl

--- src/main/java/com/rOushAn/cabcore/repositories/RatingRepository.java
+++ src/main/java/com/rOushAn/cabcore/repositories/RatingRepository.java
@@ -1,10 +1,20 @@
 package com.rOushAn.cabcore.repositories;
 
+import com.rOushAn.cabcore.entities.Driver;
 import com.rOushAn.cabcore.entities.Rating;
+import com.rOushAn.cabcore.entities.Ride;
+import com.rOushAn.cabcore.entities.Rider;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface RatingRepository extends JpaRepository<Rating, Long> {
+    List<Rating> findByRider(Rider rider);
 
+    List<Rating> findByDriver(Driver driver);
+
+    Optional<Rating> findByRide(Ride ride);
 }
--- src/main/java/com/rOushAn/cabcore/service/implementations/RatingManagementServiceImpl.java
+++ src/main/java/com/rOushAn/cabcore/service/implementations/RatingManagementServiceImpl.java
@@ -3,29 +3,93 @@ package com.rOushAn.cabcore.service.implementations;
 import com.rOushAn.cabcore.dtos.DriverDto;
 import com.rOushAn.cabcore.dtos.RiderDto;
 import com.rOushAn.cabcore.entities.Driver;
+import com.rOushAn.cabcore.entities.Rating;
 import com.rOushAn.cabcore.entities.Ride;
 import com.rOushAn.cabcore.entities.Rider;
+import com.rOushAn.cabcore.exceptions.ResourceNotFoundException;
+import com.rOushAn.cabcore.exceptions.RuntimeConflictException;
+import com.rOushAn.cabcore.repositories.DriverRepository;
+import com.rOushAn.cabcore.repositories.RatingRepository;
+import com.rOushAn.cabcore.repositories.RiderRepository;
 import com.rOushAn.cabcore.service.RatingManagementService;
+import org.apache.log4j.Logger;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 
 
 @Service
 public class RatingManagementServiceImpl implements RatingManagementService {
 
+
+    private static final Logger log = Logger.getLogger(RatingManagementServiceImpl.class);
+    private final RatingRepository ratingRepository;
+    private final DriverRepository driverRepository;
+    private final RiderRepository riderRepository;
+    private final ModelMapper modelMapper;
+
+    public RatingManagementServiceImpl(RatingRepository ratingRepository, ModelMapper modelMapper, DriverRepository driverRepository, RiderRepository riderRepository) {
+        this.ratingRepository = ratingRepository;
+        this.driverRepository = driverRepository;
+        this.riderRepository = riderRepository;
+        this.modelMapper = modelMapper;
+    }
+
     @Override
     public DriverDto rateDriver(Ride ride, Driver driver, Double rating) {
-        return null;
+        Rating ratingObj = ratingRepository
+                .findByRide(ride)
+                .orElseThrow(() -> new ResourceNotFoundException("Rating not found!"));
+        log.info(ratingObj.getDriverRating());
+
+        if (ratingObj.getDriverRating() != 0.0) throw new RuntimeConflictException("Cannot rate driver again!");
+
+        ratingObj.setDriverRating(rating);
+        ratingRepository.save(ratingObj);
+
+        Double newRating = ratingRepository
+                .findByDriver(driver)
+                .stream()
+                .mapToDouble(rating1 -> rating1.getDriverRating())
+                .average()
+                .orElse(0.0);
+        driver.setRating(newRating);
+        Driver driverSaved = driverRepository.save(driver);
+        return modelMapper.map(driverSaved, DriverDto.class);
     }
 
     @Override
     public RiderDto rateRider(Ride ride, Rider rider, Double rating) {
 
-        return null;
+        Rating ratingObj = ratingRepository
+                .findByRide(ride)
+                .orElseThrow(() -> new ResourceNotFoundException("Rating not found!"));
+
+        if (ratingObj.getRiderRating() != 0.0) throw new RuntimeConflictException("Cannot rate rider again!");
+
+        ratingObj.setRiderRating(rating);
+        ratingRepository.save(ratingObj);
+
+        Double newRating = ratingRepository
+                .findByRider(rider)
+                .stream()
+                .mapToDouble(rating1 -> rating1.getRiderRating())
+                .average()
+                .orElse(0.0);
+        rider.setRating(newRating);
+        Rider savedRider = riderRepository.save(rider);
+        return modelMapper.map(savedRider, RiderDto.class);
     }
 
     @Override
     public void createNewRating(Ride ride) {
+        Rating rating = new Rating
+                .RatingBuilder()
+                .ride(ride)
+                .rider(ride.getRider())
+                .driver(ride.getDriver())
+                .driverRating(0.0)
+                .riderRating(0.0)
+                .build();
+        ratingRepository.save(rating);
     }
-
-
 }
