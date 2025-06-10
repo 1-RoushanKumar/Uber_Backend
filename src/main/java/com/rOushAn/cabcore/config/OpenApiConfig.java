847598aef05b4ca8711b1362cbedea6d0f749c78
Roushan Kumar <rk04393@gmail.com>
2025-06-10 22:32:39 +0530

    Added OpenApi Swagger Api

---
+++ src/main/java/com/rOushAn/cabcore/config/OpenApiConfig.java
@@ -0,0 +1,30 @@
+package com.rOushAn.cabcore.config;
+
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("CabCore API")
+                        .version("1.0")
+                        .description("API documentation for CabCore – ride-hailing backend")
+                        .termsOfService("https://example.com/terms")
+                        .contact(new Contact()
+                                .name("Roushan Kumar")
+                                .email("rk04393@gmail.com")
+                                .url("https://github.com/1-RoushanKumar"))
+                        .license(new License()
+                                .name("Apache 2.0")
+                                .url("http://springdoc.org")));
+    }
+}
+
