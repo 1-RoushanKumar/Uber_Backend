91f2ad525bd038e9cfec53d46aa62e7042875968
Roushan Kumar <rk04393@gmail.com>
2025-06-10 22:11:12 +0530

    Added Validation to signDto

--- src/main/java/com/rOushAn/cabcore/dtos/SignupDto.java
+++ src/main/java/com/rOushAn/cabcore/dtos/SignupDto.java
@@ -1,9 +1,21 @@
 package com.rOushAn.cabcore.dtos;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
 public class SignupDto {
 
-    private String email;
+    @NotBlank(message = "Name cannot be blank")
+    @Size(min = 3, max = 100, message = "Name must be between 3 and 100 characters")
     private String name;
+
+    @NotBlank(message = "Email cannot be blank")
+    @Email(message = "Invalid email format")
+    private String email;
+
+    @NotBlank(message = "Password cannot be blank")
+    @Size(min = 8, message = "Password must be at least 8 characters long")
     private String password;
 
     public SignupDto() {
